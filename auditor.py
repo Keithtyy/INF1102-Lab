1d53bc4c736cd6cb566f1836bdf42ed75e9746cb
Keithtyy <2601493@sit.singaporetech.edu.sg>
2026-09-11 10:51:40 +0800

    Week2 Lab question 1

---
+++ auditor.py
@@ -0,0 +1,25 @@
+inventory = 0
+invalid_input = 0
+while True:
+    if inventory > 500:
+        print("Overstock detected, cannot add more stocks")
+        break
+    
+    stock = input("Enter current stock quantity: ")
+
+    if stock == "quit":
+        print("Total units processed: ", inventory)
+        print("Invalid inputs encountered: ", invalid_input)
+        print("Exiting the program.")
+        break
+
+    if not stock.isnumeric():
+        print("Invalid input. Please enter a numeric value.")
+        invalid_input += 1
+    elif int(stock) < 0:
+        print("Invalid input. Please enter a non-negative value.")
+        invalid_input += 1
+    
+    else:
+        inventory += int(stock)
+        print("Current stock quantity: ", inventory)
